48c2fae1eaf389b3a2bf9ab233df33c80a0cebb0
sb25104-maker <sb25104@gmail.com>
2025-08-21 10:17:24 +0530

    Add files via upload

---
+++ Assignment_1_Task1.py
@@ -0,0 +1,11 @@
+#Task 1
+
+a=input("Enter the first number:")
+b=input("Enter the second number:")
+a=int(a)
+b=int(b)
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
